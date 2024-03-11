21a09205511ccc4105eda91c4204c16278b131e6
Nasty-24 <briankibunja3@gmail.com>
2024-03-11 09:01:31 +0300

    first commit

---
+++ myapplication/serializers.py
@@ -0,0 +1,7 @@
+from myapplication.models import Employees
+from rest_framework import serializers
+
+class EmployeesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = '__all__'
---
+++ myapplication/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from myapplication.models import Employees
+from myapplication.serializers import EmployeesSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# Create your views here.
+@api_view(['GET','POST'])
+def employee_list(request):
+   if request.method == 'GET':
+    employees = Employees.objects.all()
+    serializer = EmployeesSerializer(employees, many=True)
+    return JsonResponse({'employees':serializer.data})
+   if request.method == 'POST':
+     serializer = EmployeesSerializer(data=request.data)
+     if serializer.is_valid():
+         serializer.save()
+         return Response(serializer.data, status=status.HTTP_201_CREATED)
